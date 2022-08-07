0f006dcb6cd8ad49fb680dc2508797db85017695
Natali <n_sunberry@inbox.ru>
2022-08-07 08:11:00 +0300

    Example005

---
+++ Examples/Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ведите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "натали")
+{ 
+    Console.WriteLine("Это же Натали!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+    }
