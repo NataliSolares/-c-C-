786b03d4381e630e407dc8cd73da145d875e4fe2
Natali <n_sunberry@inbox.ru>
2022-08-07 11:58:39 +0300

    Example007

---
+++ Examples/Example007/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+//Console.BufferHeight = 80;
+//Console.BufferWidth = 80;
+//Console.SetCursorPosition(10, 4);
+Console.WriteLine("+");
+
+int xa = 1, ya = 1,
+    xb = 1, yb = 25,
+    xc = 30, yc = 30;
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
